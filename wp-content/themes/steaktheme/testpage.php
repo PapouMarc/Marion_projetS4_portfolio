14f43beaa8ad94c813db2d81ecd89b4cd883bc95
PapouMarc <gamermarc090@gmail.com>
2024-03-17 21:52:30 +0100

    update

--- wp-content/themes/steaktheme/testpage.php
+++ wp-content/themes/steaktheme/testpage.php
@@ -29,7 +29,7 @@
  
 
      //Pour afficher la liste des catégores en format html
-    $cat_taxonomie = 'category';
+    $cat_taxonomie = 'articlemmi';
     $args_cat = array(
 		'echo'                => 0,
 		'style'               => '', //mettre liste à la place de '' pour avoir un formatage ul
@@ -44,8 +44,8 @@
     Pour recupérer la data brute à partir du nom, permet de recupérer aussi les sous-cats
 
     */
-    $cat_name = 'Cours';
-    $cat_taxonomie = 'category';
+    $cat_name = 'animation';
+    $cat_taxonomie = 'articlemmi';
     $terms = array(
         'name' => $cat_name, //pour trier par nom
         'taxonomy' => $cat_taxonomie
@@ -68,8 +68,8 @@
     /*Pour recupérer la data brute de la table taxonomy en fonction de la description
     https://developer.wordpress.org/reference/functions/get_terms/
     */
-    $cat_description = 'Programme';
-    $cat_taxonomie = 'category';
+    $cat_description = '';
+    $cat_taxonomie = 'articlemmi';
     $terms = get_terms( array(
         'taxonomy' => $cat_taxonomie,
         'hide_empty' => false,
@@ -95,8 +95,8 @@
     /*Pour recupérer la data brute de la table taxonomy en fonction du slug
     A partir de fonction interne wordpress
     */
-    $cat_slug = 'cryptomonnaie';
-    $cat_taxonomie = 'category';
+    $cat_slug = 'animation';
+    $cat_taxonomie = 'articlemmi';
     $cat_detail = get_category_by_slug_marion( $cat_slug,$cat_taxonomie );
     echo '<hr> list by get_category_by_slug_marion<br/>';
     print_r($cat_detail );
@@ -118,25 +118,25 @@
     https://developer.wordpress.org/reference/classes/wp_query/#Category_Parameters
     */
     
-    $cat_slug = 'physique-chimie';
-    $cat_taxonomie = 'category';
-    $cat_detail = get_category_by_slug_marion( $cat_slug,$cat_taxonomie );
-    echo '<hr> list by get_category_by_slug_marion<br/>';
-    print_r($cat_detail->term_id);
-    echo '<br/>';
+    $cat_taxonomie = 'articlemmi';
     $args = array (
         'showposts' => '20',
-        'cat' => $cat_detail->term_id,
+        // 'cat' => $cat_detail->term_id,
         'tax_query' => array(
             array(
                 'taxonomy' => $cat_taxonomie,
-                'operator' => 'EXISTS'
+                // 'operator' => 'EXISTS',
+                'field'    => 'slug',
+                'terms'    => array( 'animation','design' )
+
             )),
     );
     $the_query = new WP_query( $args );
+// print_r($the_query);
     echo '<hr><br/>Nombre de post:';
     print_r($the_query->have_posts());
     echo '<br/>';
+
     if ( $the_query->have_posts() ){
         while ( $the_query->have_posts() ){
             $the_query->the_post();
