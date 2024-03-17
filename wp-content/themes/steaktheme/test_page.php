ea72841166a12aadce99de222535d21e0bc8cbc7
PapouMarc <gamermarc090@gmail.com>
2024-03-17 16:48:11 +0100

    exemple d'utilisation de fonction wordpress

---
+++ wp-content/themes/steaktheme/test_page.php
@@ -0,0 +1,117 @@
+<?php
+    /*
+    Template Name: test_page
+    */
+    define( 'WP_DEBUG', true ); // permet de forcer le debugage de la page en cas d'erreur
+     echo 'Page de debug<br/><br/>';
+
+     //Pour afficher la liste des catégores en format html
+    $args_cat = array(
+		'echo'                => 0,
+		'style'               => '', //mettre liste à la place de '' pour avoir un formatage ul
+		'taxonomy'            => 'category',
+	);     
+    $list_cats = wp_list_categories( $args = $args_cat );
+    echo '<hr> list by wp_list_categories<br/>';
+    print_r($list_cats);
+
+
+    /*
+    Pour recupérer la data brute à partir du nom, permet de recupérer aussi les sous-cats
+
+    */
+    $cat_name = 'Cours';
+    $terms = array(
+        'name' => $cat_name //pour trier par nom
+    );
+    $cat_detail = get_categories( $args = $terms );
+    echo '<hr> list by get_categories<br/>';
+    print_r($cat_detail[0] );
+    echo '<br/>';
+    //trap l id
+    echo 'Id categorie '. $cat_slug .':';
+    print_r($cat_detail[0]->term_id);
+    echo '<br/>';
+    //trap la description
+    echo 'Description categorie '. $cat_slug .':';
+    print_r($cat_detail[0]->description);
+    echo '<br/>';
+
+
+
+    /*Pour recupérer la data brute de la table taxonomy en fonction de la description
+    https://developer.wordpress.org/reference/functions/get_terms/
+    */
+    $cat_description = 'Programme';
+    $terms = get_terms( array(
+        'taxonomy' => 'category',
+        'hide_empty' => false,
+        'orderby' => 'name',
+        'order' => 'ASC',
+        'description__like' => $cat_description // pour trier par description
+        //'name' => $cat_name //pour trier par nom
+        //'slug' => $cat_slug //pour trier par slug
+    ) );
+    echo '<hr> list by terms<br/>';
+    print_r($terms );
+    echo '<br/>';
+    //trap l id
+    echo 'Id categorie '. $cat_description .':';
+    print_r($terms[0]->term_id);
+    echo '<br/>';
+    //trap la description
+    echo 'Description categorie '. $cat_description .':';
+    print_r($terms[0]->description);
+    echo '<br/>';
+
+
+    /*Pour recupérer la data brute de la table taxonomy en fonction du slug
+    A partir de fonction interne wordpress
+    */
+    $cat_slug = 'cryptomonnaie';
+    $cat_detail = get_category_by_slug( $cat_slug );
+    echo '<hr> list by get_category_by_slug<br/>';
+    print_r($cat_detail );
+    echo '<br/>';
+    //trap l id
+    echo 'Id categorie '. $cat_slug .':';
+    print_r($cat_detail->term_id);
+    echo '<br/>';
+    //trap la description
+    echo 'Description categorie '. $cat_slug .':';
+    print_r($cat_detail->description);
+    echo '<br/>';
+
+
+
+
+    /*
+    lister un post spécifique à une categorie
+    https://developer.wordpress.org/reference/classes/wp_query/#Category_Parameters
+    */
+    
+    $cat_slug = 'physique-chimie';
+    $cat_detail = get_category_by_slug( $cat_slug );
+    echo '<hr> list by get_category_by_slug<br/>';
+    print_r($cat_detail );
+    echo '<br/>';
+    $args = array (
+        'showposts' => '20',
+        'cat' => $cat_detail->term_id
+    );
+    $the_query = new WP_Query( $args );
+    echo '<hr><br/>Nombre de post:';
+    print_r($the_query->have_posts());
+    echo '<br/>';
+    if ( $the_query->have_posts() ){
+        while ( $the_query->have_posts() ){
+            $the_query->the_post();
+            the_title();
+            echo '<br/>';
+            the_permalink();
+            echo '<br/>';
+            the_content();
+       }
+    }
+    wp_reset_postdata();
+?>
